08f1c4be9d75bbfab6dc93d089b73c71e151bc3d
zulfuakgunes <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>
2024-02-16 16:55:36 +0300

    textfield guide

--- UIKitComponentsBox/TextFieldViewController.swift
+++ UIKitComponentsBox/TextFieldViewController.swift
@@ -6,6 +6,29 @@
 //
 
 import UIKit
-class TextFieldViewController: UIViewController {
+class TextFieldViewController: UIViewController, UITextFieldDelegate {
+    lazy var nativeTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.placeholder = "Hello world"
+        textField.font = UIFont.systemFont(ofSize: 15)
+        textField.borderStyle = .roundedRect
+        textField.textColor = .darkGray
+        textField.delegate = self //textfield delegate set
+        
+        textField.returnKeyType = .done
+        textField.addTarget(self, action: #selector(returnedPressed), for: .editingDidEnd)
+        
+        view.addSubview(textField)
+        return textField
+    }()
     
+    @objc func returnedPressed() {
+        nativeTextField.becomeFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nativeTextField.becomeFirstResponder()
+        return true
+    }
 }
